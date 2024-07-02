2a2707b3016d9d1cce64d8e71d8d1d5aca2ab02e
Aiden Sanders <aidensandersbiz@gmail.com>
2024-07-02 10:15:31 -0600

    Input Class

--- Engine/Source/Vector2.h
+++ Engine/Source/Vector2.h
@@ -8,7 +8,7 @@ struct Vector2
 
 	Vector2() = default;
 	Vector2(float x, float y) { this->x = x; this->y = y; }
-	Vector2(int x, int y) { this->x = x; this->y = y; }
+	Vector2(int x, int y) { this->x = (float)x; this->y = (float)y; }
 
 	Vector2 operator + (Vector2 v) const { return Vector2{ x + v.x, y + v.y }; }
 	Vector2 operator - (Vector2 v) const { return Vector2{ x - v.x, y - v.y }; }
@@ -20,6 +20,6 @@ struct Vector2
 	Vector2 operator * (float s) const { return Vector2{ x * s, y * s }; }
 	Vector2 operator / (float s) const { return Vector2{ x / s, y / s }; }
 
-	float LengthSqr(Vector2 v1, Vector2 v2) { return (v1.x * v2.x) + (v1.y * v2.y); }
-	float Length(Vector2 v1, Vector2 v2) { return std::sqrt((v1.x * v2.x) + (v1.y * v2.y)); }
+	float LengthSqr() { return (x * x) + (y * y); }
+	float Length() { return std::sqrt((x * x) + (y * y)); }
 };
--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,5 +1,6 @@
 #include "Renderer.h"
 #include "Vector2.h"
+#include "Input.h"
 
 #include <SDL.h>
 #include <iostream>
@@ -7,32 +8,58 @@
 
 int main(int argc, char* argv[])
 {
+	// create systems
 	Renderer renderer;
 	renderer.Initialize();
 	renderer.CreateWindow("Game Engine", 800, 600);
 	
+	Input input;
+	input.Initialize();
+
 	Vector2 v1{ 400, 300 };
 	Vector2 v2{ 500, 400 };
 
 	std::vector<Vector2> points;
-	for (int i = 0; i < 100; i++)
+	/*for (int i = 0; i < 100; i++)
 	{
 		points.push_back(Vector2{ rand() % 800, rand() % 600 });
-	}
+	}*/
 
 	//main
-	while (true)
+	bool quit = false;
+	while (!quit)
 	{
 		// input
 		// update
 		// draw
+
+		// INPUT
+		input.Update();
+
+		if (input.GetKeyDown(SDL_SCANCODE_ESCAPE))
+		{
+			quit = true;
+		}
 		
 		// UPDATE
-		Vector2 speed{ 0.1f, -0.1f };
+		Vector2 mousePosition = input.GetMousePosition();
+		//std::cout << mousePosition.x << " " << mousePosition.y << std::endl;
+
+		if (input.GetMouseButtonPressed(0)) 
+		{
+			std::cout << "mouse pressed\n";
+			points.push_back(mousePosition);
+		}
+		if (input.GetMouseButtonHeld(0)) {
+			float distance = (points.back() - mousePosition).Length();
+			if (distance > 5) points.push_back(mousePosition);
+		}
+
+		/*Vector2 speed{ 0.1f, -0.1f };
 		for (Vector2& point : points)
 		{
 			point = point + 0.02f;
-		}
+		}*/
 
 
 		// DRAW
@@ -41,32 +68,33 @@ int main(int argc, char* argv[])
 		renderer.BeginFrame();
 		
 		// draw shape 
-		renderer.SetColor(255, 255, 255, 0); 
-		renderer.DrawLine(400, 150, 500, 450);
+		//renderer.SetColor(255, 255, 255, 0); 
+		/*renderer.DrawLine(400, 150, 500, 450);
 		renderer.DrawLine(500, 450, 250, 250);
 		renderer.DrawLine(250, 250, 550, 250);
 		renderer.DrawLine(550, 250, 300, 450);
 		renderer.DrawLine(300, 450, 400, 150);
-		renderer.DrawLine(v1.x, v1.y, v2.x, v2.y);
+		renderer.DrawLine(v1.x, v1.y, v2.x, v2.y);*/
 
-		for (int i = 0; i < points.size() - 1; i++)
+		for (int i = 0; points.size() > 1 && i < points.size() - 1; i++)
 		{
+			renderer.SetColor(rand() % 256, rand() % 256, rand() % 256, 0);
 			renderer.DrawLine(points[i], points[i + 1]);
 		}
 
 		// random lines
-		for (int i = 0; i < rand() % 300; i++)
+		/*for (int i = 0; i < rand() % 300; i++)
 		{
 			renderer.SetColor(rand() % 256, rand() % 256, rand() % 256, 0);
 			renderer.DrawLine(rand() % 800, rand() % 600, rand() % 800, rand() % 600);
-		}
+		}*/
 
 		// random dots
-		for (int i = 0; i < rand() % 5000; i++)
+		/*for (int i = 0; i < rand() % 5000; i++)
 		{
 			renderer.SetColor(rand() % 256, rand() % 256, rand() % 256, 0);
 			renderer.DrawPoint(rand() % 800, rand() % 600);
-		}
+		}*/
 
 		// show screen
 		renderer.EndFrame();
